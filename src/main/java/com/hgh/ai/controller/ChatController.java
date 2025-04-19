30eb2180187e45946cf2e94591f04592ce44ba55
hgh66 <18319523592@163.com>
2025-04-19 18:27:47 +0800

    AI聊天功能实现

---
+++ src/main/java/com/hgh/ai/controller/ChatController.java
@@ -0,0 +1,76 @@
+package com.hgh.ai.controller;
+
+import com.hgh.ai.repository.ChatHistoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.model.Media;
+import org.springframework.util.MimeType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/ai")
+public class ChatController {
+
+    private final ChatClient chatClient;
+    private final ChatHistoryRepository chatHistoryRepository;
+
+    @RequestMapping(value = "/chat", produces = "text/html;charset=utf-8")
+    public Flux<String> chat(
+            @RequestParam("prompt") String prompt,
+            @RequestParam("chatId") String chatId,
+            @RequestParam(value = "files", required = false) List<MultipartFile> files) {
+        // 1.保存会话id
+        chatHistoryRepository.save("chat", chatId);
+        // 2.请求模型
+        if (files == null || files.isEmpty()) {
+            // 没有附件，纯文本聊天
+            return textChat(prompt, chatId);
+        } else {
+            // 有附件，多模态聊天
+            return multiModalChat(prompt, chatId, files);
+        }
+
+    }
+
+    /**
+     * 处理多模态聊天请求的方法
+     * 该方法接收用户提示文本、聊天ID和多媒体文件列表，然后解析多媒体文件并将其与文本提示一起发送给聊天模型
+     * 主要用于处理包含多媒体内容的聊天场景，如图片、语音等
+     */
+    private Flux<String> multiModalChat(String prompt, String chatId, List<MultipartFile> files) {
+        // 1.解析多媒体
+        // 将上传的多媒体文件转换为Media对象列表，以便后续处理
+        List<Media> medias = files.stream()
+                .map(file -> new Media(
+                                MimeType.valueOf(Objects.requireNonNull(file.getContentType())),
+                                file.getResource()
+                        )
+                )
+                .toList();
+        // 2.请求模型
+        // 构建用户提示，包括文本和多媒体内容，并调用模型进行聊天，返回模型的回复流
+        return chatClient.prompt()
+                .user(p -> p.text(prompt).media(medias.toArray(Media[]::new)))
+                .advisors(a -> a.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId))
+                .stream()
+                .content();
+    }
+
+    private Flux<String> textChat(String prompt, String chatId) {
+        return chatClient.prompt()
+                .user(prompt)
+                .advisors(a -> a.param(CHAT_MEMORY_CONVERSATION_ID_KEY, chatId))
+                .stream()
+                .content();
+    }
+}
